5056f471295250205bb90683bdd3334cf5ebf582
sree ram <sreerameppalapalli@gmail.com>
2023-08-09 23:53:07 +0530

    secondary

---
+++ lib/answer_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Answer_button extends StatelessWidget{
+  Answer_button(this.Ontap,this.AnswerText,{super.key});
+  final String AnswerText;
+  final void Function() Ontap;
+  @override
+  Widget build(context){
+    return  ElevatedButton(
+        onPressed:Ontap,
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.purple,
+            foregroundColor: Colors.white,padding: EdgeInsets.symmetric(vertical:10,horizontal: 100),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
+        child:  Text(AnswerText),
+    );
+  }
+
+}
---
+++ lib/model/quiz_question.dart
@@ -0,0 +1,5 @@
+class QuizQuestion{
+ const  QuizQuestion(this.text,this.answers);
+  final text;
+  final List<String> answers;
+}
